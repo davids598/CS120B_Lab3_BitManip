d80b6627ed840cd90bc66e8df39f9e2f0a264cff
David <davidstrathman@gmail.com>
2021-10-09 19:46:53 -0700

    Finished Part 3

---
+++ turnin/dstra006_lab3_part3_tests.py
@@ -0,0 +1,48 @@
+# Array of tests to run (in order)
+# Each test contains
+#   description -
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.)
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or
+# altered in between executions (unless preconditions are used).
+
+tests = [ {'description': 'PINA: 0x30 => PORTC: 0xC0',
+    'steps': [ {'inputs': [('PINA',0x30)], 'iterations': 5 } ],
+    'expected': [('PORTC',0xC0)],
+    },
+    {'description': 'PINA: 0x01 => PORTC: 0x60',
+    'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x60)],
+    },
+    {'description': 'PINA: 0x33 => PORTC: 0xF0',
+    'steps': [ {'inputs': [('PINA',0x33)], 'iterations': 5 } ],
+    'expected': [('PORTC',0xF0)],
+    },
+    {'description': 'PINA: 0x3F => PORTC: 0xBF',
+    'steps': [ {'inputs': [('PINA',0x3F)], 'iterations': 5 } ],
+    'expected': [('PORTC',0xBF)],
+    },
+    {'description': 'PINA: 0x7A => PORTC: 0x3E',
+    'steps': [ {'inputs': [('PINA',0x0A)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x3E)],
+    },
+    {'description': 'PINA: 0x2D => PORTC: 0x3F',
+    'steps': [ {'inputs': [('PINA',0x0D)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x3F)],
+    },
+    ]
+watch = ['PORTC']
+
+
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The
+# variables listed here will display everytime you hit (and stop at) a breakpoint
